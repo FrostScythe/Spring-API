3e38627f8b0e3d0f2be7a4601280d4d7432d6fb2
FrostScythe <maheshwariayush371@gmail.com>
2025-08-03 18:56:37 +0530

    Basic API setup

---
+++ src/main/java/com/peoject/API/ApiApplication.java
@@ -0,0 +1,69 @@
+package com.peoject.API;
+
+import com.peoject.API.model.Address;
+import com.peoject.API.model.Employee;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // We can only contain oe main method  and this annotation will only be written in main file.
+// learn about the structure of project.
+// what are the feature of java?  what are thr feature of spring?
+
+public class ApiApplication  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+		System.out.println("Hello World this is our first spring boot application.");
+
+		// tight coupling
+		/*
+		Employee emp1= new Employee();
+		emp1.id=123;
+		emp1.name= "Ajay";
+		emp1.email = "aiaj123@g.com";
+		emp1.designation="developer";
+
+		Address adrs1= new Address();
+		adrs1.doorNo=99;
+		adrs1.streetName= "lk";
+		adrs1.city="ejo";
+
+		emp1.address = adrs1;
+
+		System.out.println("Emp1  is: "+ emp1);
+		System.out.println("Adrs1  is: "+ adrs1);
+
+		adrs1.streetame ="new street";
+		 */
+
+		//loose coupling
+		// setter injection
+		Employee emp2 = new Employee();
+		emp2.setId(2345);
+		emp2.setName("abra");
+		emp2.setEmail("abra@dabra.com");
+		emp2.setDesignation("developer");
+
+		Address adrs2 = new Address();
+		adrs2.setDoorNo(345);
+		adrs2.setStreetName("dholakPur");
+		adrs2.setCity("Pogo");
+
+		emp2.setAddress(adrs2);
+
+		System.out.println("Emp2  is: "+ emp2);
+		System.out.println("Adrs2  is: "+ adrs2);
+
+		emp2.getAddress().setStreetName("new street");
+
+		// constructor ijuction
+		// need to pass the data in same order
+		Address adrs3= new Address(456, "old street","old city");
+		Employee emp3= new Employee(234,"sam", "sam@g.com","developer",adrs3);
+
+		System.out.println("Emp3  is: "+ emp3);
+		System.out.println("Adrs3 is: "+ adrs3);
+
+		emp3.getAddress().setStreetName("new street");
+	}
+}
---
+++ src/main/java/com/peoject/API/model/Address.java
@@ -0,0 +1,50 @@
+package com.peoject.API.model;
+
+public class Address {
+    // for tight  copulig we make all of them public ad for loose copulig we will make all of them private
+    private int doorNo;
+    private String streetName;
+    private String city;
+
+    public Address(int doorNo, String streetName, String city) {
+        this.doorNo = doorNo;
+        this.streetName = streetName;
+        this.city = city;
+    }
+
+    public Address() {
+    }
+
+    public int getDoorNo() {
+        return doorNo;
+    }
+
+    public void setDoorNo(int doorNo) {
+        this.doorNo = doorNo;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "doorNo=" + doorNo +
+                ", streetName='" + streetName + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
