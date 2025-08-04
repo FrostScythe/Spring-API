00bc8176934363183b7da0f327fad28d65670c61
FrostScythe <maheshwariayush371@gmail.com>
2025-08-04 23:58:44 +0530

    Get and request mapping

--- src/main/java/com/peoject/API/controller/DemoController.java
+++ src/main/java/com/peoject/API/controller/DemoController.java
@@ -1,7 +1,17 @@
 package com.peoject.API.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/demo/api")
+
 public class DemoController {
+    @GetMapping
+    public String getDemo(){
+        System.out.println("This is our first demo API");
+        return "This is our first demo API";
+    }
 }
--- src/main/java/com/peoject/API/controller/SampleController.java
+++ src/main/java/com/peoject/API/controller/SampleController.java
@@ -1,7 +1,11 @@
 package com.peoject.API.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+//@Controller
+//@ResponseBody // used to send the response to front end
+@RestController //combination of two annotation @Controller+ @ResponseBody
 public class SampleController {
 }
